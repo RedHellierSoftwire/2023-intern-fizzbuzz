49ffb5471926627d4dea1ddbbc594ddc8b45f4d9
RedHellierSoftwire <red.hellier@softwire.com>
2025-09-10 10:16:20 +0100

    Added isMult check function, utilised function for 3 and 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,24 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
 
-    // Put your code here...
+    
 
+    for (let n = 1; n <= 100; n++) {
+        const fizzyArray = [];
+        let fizzyOutput = '';
+
+        if (isMult(n, 3)) { fizzyArray.push('Fizz'); }
+        if (isMult(n, 5)) { fizzyArray.push('Buzz'); }
+
+        fizzyOutput = fizzyArray.length > 0 ? fizzyArray.join('') : n
+
+        console.log(fizzyOutput);
+    }
+
+}
+
+const isMult = (n,m) => {
+    return n%m === 0;
 }
 
 // Now, we run the main function:
