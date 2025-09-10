45f3b1bb07d549f04786073dadf84a018bf07091
RedHellierSoftwire <red.hellier@softwire.com>
2025-09-10 10:46:23 +0100

    Added functionality for 7, 11, 13, 17

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,14 +3,21 @@ function fizzbuzz() {
 
     
 
-    for (let n = 1; n <= 100; n++) {
-        const fizzyArray = [];
+    for (let n = 0; n <= 255; n++) {
+        let fizzyArray = [];
         let fizzyOutput = '';
 
         if (isMult(n, 3)) { fizzyArray.push('Fizz'); }
         if (isMult(n, 5)) { fizzyArray.push('Buzz'); }
+        if (isMult(n, 7)) { fizzyArray.push('Bang'); }
+        if (isMult(n, 11)) { fizzyArray = ['Bong']; }
+        if (isMult(n, 13)) { 
+            const firstBIndex = fizzyArray.findIndex(word => /^B/.test(word));
+            firstBIndex === -1 ? fizzyArray.push('Fezz') : fizzyArray.splice(firstBIndex,0,'Fezz');
+        }
+        if (isMult(n, 17)) { fizzyArray.reverse() }
 
-        fizzyOutput = fizzyArray.length > 0 ? fizzyArray.join('') : n
+        fizzyOutput = fizzyArray.length > 0 ? fizzyArray.join('') : n;
 
         console.log(fizzyOutput);
     }
