9196c8e942e4edc566c2ebcaa2742387527a9d39
RedHellierSoftwire <red.hellier@softwire.com>
2025-09-10 12:01:36 +0100

    Clearer function and variable names and comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,32 +1,42 @@
-// This is our main function
 function fizzbuzz() {
-
-    
-
     for (let n = 0; n <= 255; n++) {
         let fizzyArray = [];
         let fizzyOutput = '';
 
-        if (isMult(n, 3)) { fizzyArray.push('Fizz'); }
-        if (isMult(n, 5)) { fizzyArray.push('Buzz'); }
-        if (isMult(n, 7)) { fizzyArray.push('Bang'); }
-        if (isMult(n, 11)) { fizzyArray = ['Bong']; }
-        if (isMult(n, 13)) { 
-            const firstBIndex = fizzyArray.findIndex(word => /^B/.test(word));
-            firstBIndex === -1 ? fizzyArray.push('Fezz') : fizzyArray.splice(firstBIndex,0,'Fezz');
-        }
-        if (isMult(n, 17)) { fizzyArray.reverse() }
+        // Check Divisibility and Apply each Rule
+        if (isMultiple(n, 3)) { fizzyArray.push('Fizz'); }
+        if (isMultiple(n, 5)) { fizzyArray.push('Buzz'); }
+        if (isMultiple(n, 7)) { fizzyArray.push('Bang'); }
+        if (isMultiple(n, 11)) { fizzyArray = ['Bong']; }
+        if (isMultiple(n, 13)) {  insertFezzBeforeFirstBWord(fizzyArray) }
+        if (isMultiple(n, 17)) { fizzyArray.reverse() }
 
+        // Convert fizzyArray to string output OR original number
         fizzyOutput = fizzyArray.length > 0 ? fizzyArray.join('') : n;
 
         console.log(fizzyOutput);
     }
-
 }
 
-const isMult = (n,m) => {
-    return n%m === 0;
+/**
+ * 
+ * @param {number} divisor 
+ * @param {number} dividend 
+ * @returns A boolean for whether the divisor is a multiple of the dividend
+ */
+const isMultiple = (divisor,dividend) => {
+    return divisor%dividend === 0;
 }
+
+/**
+ * Inserts the word Fezz before the first word beginning with B OR at the end of no B word exists
+ * @param {Array} fizzyArray 
+ */
+const insertFezzBeforeFirstBWord = (fizzyArray) => {
+    const firstBIndex = fizzyArray.findIndex(word => /^B/.test(word));
+    firstBIndex === -1 ? fizzyArray.push('Fezz') : fizzyArray.splice(firstBIndex,0,'Fezz');
+}
+
 
 // Now, we run the main function:
 fizzbuzz();
